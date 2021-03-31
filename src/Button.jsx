5c9e01c787bdfac9d26d7ad595142c3f7c7fa6e4
Vlad-maker <skateislife@mail.ru>
2021-03-31 19:18:45 +0300

    Feat: Button & Header components added

---
+++ src/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const Button = (props) => {
+    return ( 
+        <button className={classNames('button', {
+            'button--outline': props.outline,
+        })}>
+        {props.children}
+        </button>
+    );
+};
+
+
+
+export default Button;
